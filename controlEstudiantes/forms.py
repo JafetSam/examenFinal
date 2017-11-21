c1f397955de89bd03be100baf2e669073241fbb8
JafetSam <JafetSam7@gmail.com>
2017-11-21 13:19:49 -0600

    tercera

--- controlEstudiantes/forms.py
+++ controlEstudiantes/forms.py
@@ -18,7 +18,7 @@ class GradoForm(forms.ModelForm):
 
 def __init__ (self, *args, **kwargs):
 
-        super(MateriaForm, self).__init__(*args, **kwargs)
+        super(GradoForm, self).__init__(*args, **kwargs)
         self.fields["materias"].widget = forms.widgets.CheckboxSelectMultiple()
         self.fields["materias"].help_text = "Ingrese el grado"
-        self.fields["materias"].queryset = Grado.objects.all()
+        self.fields["materias"].queryset = Materia.objects.all()
